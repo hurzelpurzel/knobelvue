5881f8da01b4435caa7670f1e1a0440eae3016e2
ludger.pottmeier@atos.net <ludger.pottmeier@atos.net>
2019-07-11 14:15:14 +0200

    better

--- app.js
+++ app.js
@@ -1,45 +1,50 @@
+"use strict";
+const storageid = 'spielstore';
+var cfg = {
+  persist: false 
+}
+
 class Werte {
   constructor(typ, boylie) {
-    this.typ=typ;
-    this.boylie=boylie;
+    this.typ = typ;
+    this.boylie = boylie;
     this.counter = 0;
   }
-  berechne() {
-    switch (this.typ) {
-      case 'Runde': return this.counter * 2.5; break;
-      case 'AK': return this.counter * 5; break;
-      case 'Hälfte': return this.counter * 0.5; break;
-      case 'Out': return this.counter * 1; break;
-      case 'Out Alle': return this.counter * 1; break;
-      case 'Drei Harte': return this.counter * 1; break;
-      case 'Strafen': return this.counter * 0.5; break;
-    }
-  }
+  
 }
+
+
+/*----------------------------------*/
 Vue.filter('euro', function (value) {
-  if (!value) return  '0.00 €'
-  result = value * 0.5;
-  return result.toString() +' €';
+  if (!value) return '0.00 €'
+  let result = value * 0.5;
+  return result.toString() + ' €';
 })
+/*----------------------------------*/
 // Define a new component called button-counter
 Vue.component('button-counter', {
   props: ['wert'],
   data: function () {
     return {
-     
+
       counter: 0
     }
   },
-  
+  beforeMount: function(){
+    this.sync();
+  },
   methods: {
+    sync: function () {
+      this.counter = this.wert.counter;
+    },
     inc: function () {
       this.wert.counter += 1;
-      this.counter = this.wert.counter;
+      this.sync();
     },
 
     dec: function () {
       this.wert.counter -= 1;
-      this.counter = this.wert.counter;
+      this.sync();
     }
   },
   template: '<span><button class="btn btn-outline-success" v-on:click="inc">+</button> \
@@ -47,40 +52,62 @@ Vue.component('button-counter', {
   <button class="btn btn-outline-danger"  v-on:click="dec">-</button> </span>'
 })
 
-
+/*----------------------------------*/
 var app = new Vue({
-  el: '#header',
+  el: '#app',
   data: {
-    greeting: 'Hallo Boylies !'
-
-  },
-  methods: {
-    humanizeURL: function (url) {
-      return url
-        .replace(/^https?:\/\//, '')
-        .replace(/\/$/, '')
-    }
-  }
-})
-var content = new Vue({
-  el: '#content',
-  data: {
+    spiel: { jahr: '', monat: '', gastgeber: '' },
     boylies: ['Uwe', 'Fedde', 'Hubi', 'Ludger', 'Schnitte', 'Guido'],
     typen: ['Runde', 'AK', 'Hälfte', 'Out', 'Out Alle', 'Drei Harte', 'Strafen'],
     bewertung: {
-      'Uwe': new Array(), 'Fedde': new Array(), 'Hubi': new Array(), 'Ludger': new Array(),
-      'Schnitte': new Array(), 'Guido': new Array()
+      'Uwe': {}, 'Fedde': {}, 'Hubi': {}, 'Ludger': {},
+      'Schnitte': {}, 'Guido':  {}
     },
     summe: {
       'Uwe': 0, 'Fedde': 0, 'Hubi': 0, 'Ludger': 0,
-      'Schnitte': 0, 'Guido': 0, 'Gesamt':0
+      'Schnitte': 0, 'Guido': 0, 'Gesamt': 0
     }
+  },
+  beforeMount: function () {
+    this.init();
+    this.load();
 
   },
-
-
   methods: {
-    calcAll: function(){
+    save: function () {
+      let comp = this.composite();
+      if(cfg.persist){
+        window.localStorage.setItem(storageid, JSON.stringify(comp));  
+      }else{
+        window.sessionStorage.setItem(storageid, JSON.stringify(comp));
+
+      }
+          },
+    load: function () {
+      let spielStr = cfg.persist ? window.localStorage.getItem(storageid):window.sessionStorage.getItem(storageid);
+      if (spielStr) {
+        let comp=JSON.parse(spielStr);
+        this.setComposite(comp);
+      } 
+    
+    },
+    composite: function(){
+        return {
+         spiel: this.spiel,
+         bewertung: this.bewertung,
+         summe: this.summe
+       };
+    },
+    setComposite: function(comp){
+        if(comp){
+          this.spiel = comp.spiel;
+          this.bewertung = comp.bewertung;
+          this.summe= comp.summe;
+        }
+       
+    },
+    
+    calcAll: function () {
       for (let index = 0; index < this.boylies.length; index++) {
         const name = this.boylies[index];
         this.calc(name);
@@ -89,23 +116,33 @@ var content = new Vue({
     calc: function (boylie) {
       let result = 0.0;
       let arr = this.bewertung[boylie];
-      for(var propt in arr){
+      for (var propt in arr) {
         const element = arr[propt];
-        result += element.berechne();
+        result += this.berechne(element);
       }
-      this.summe[boylie]=result;
-    }
-  },
-  beforeMount: function () {
-    for (let index = 0; index < this.boylies.length; index++) {
-      const name = this.boylies[index];
-      for (let index1 = 0; index1 < this.typen.length; index1++) {
-        const typ = this.typen[index1];
+      this.summe[boylie] = result;
+    },
+    berechne(element) {
+      switch (element.typ) {
+        case 'Runde': return element.counter * 2.5; break;
+        case 'AK': return element.counter * 5; break;
+        case 'Hälfte': return element.counter * 0.5; break;
+        case 'Out': return element.counter * 1; break;
+        case 'Out Alle': return element.counter * 1; break;
+        case 'Drei Harte': return element.counter * 1; break;
+        case 'Strafen': return element.counter * 0.5; break;
+      }
+    },
+    
+    init() {
+      for (let index = 0; index < this.boylies.length; index++) {
+        const name = this.boylies[index];
+        for (let index1 = 0; index1 < this.typen.length; index1++) {
+          const typ = this.typen[index1];
 
-        this.bewertung[name][typ] = new Werte(typ, name);
+          this.bewertung[name][typ] = new Werte(typ, name);
+        }
       }
     }
-
-
   }
 })
